79b7ba5c246634be6e76a5e47d79ae2ea7589e81
jack3 <41743337+jack3-github@users.noreply.github.com>
2025-07-02 13:59:37 +0900

    data mod

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 ﻿
 const flashcardsData = [
     { "questionImage": "images/日産 スカイライン GT-Rパトロールカー.jpg", "answerText": "日産 スカイライン GT-Rパトロールカー" },
-    { "questionImage": "images/日産 スカイライン GT-Rパトロールカー.webp", "answerText": "日産 スカイライン GT-Rパトロールカー" },
+    { "questionImage": "images/1日産 スカイライン GT-R パトロールカー.webp", "answerText": "日産 スカイライン GT-Rパトロールカー" },
     { "questionImage": "images/スバル WRX S4 覆面パトロールカー.webp", "answerText": "スバル WRX S4 覆面パトロールカー" },
 ];
